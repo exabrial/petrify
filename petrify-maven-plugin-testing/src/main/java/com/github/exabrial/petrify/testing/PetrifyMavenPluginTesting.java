5204d9ee1a5f6103d4f0090f569f6cd52c3805b0
Jonathan S. Fisher <exabrial@gmail.com>
2026-04-14 17:16:21 -0500

    how far does the silliness go

---
+++ petrify-maven-plugin-testing/src/main/java/com/github/exabrial/petrify/testing/PetrifyMavenPluginTesting.java
@@ -0,0 +1,9 @@
+package com.github.exabrial.petrify.testing;
+
+/**
+ * Marker class for the petrify-maven-plugin-testing module.
+ */
+public final class PetrifyMavenPluginTesting {
+	private PetrifyMavenPluginTesting() {
+	}
+}
---
+++ petrify-testing/src/main/java/com/github/exabrial/petrify/testing/PetrifyTesting.java
@@ -0,0 +1,9 @@
+package com.github.exabrial.petrify.testing;
+
+/**
+ * Marker class for the petrify-testing module.
+ */
+public final class PetrifyTesting {
+	private PetrifyTesting() {
+	}
+}
