af6c9fc3f909f455c50ecf0b3ff9e872bf7d8fd0
Jonathan S. Fisher <exabrial@gmail.com>
2026-04-14 17:14:43 -0500

    add empty files so javadoc jars get created

---
+++ petrify-maven-plugin-testing/src/main/java/com/github/exabrial/petrify/testing/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Integration tests for the petrify-maven-plugin.
+ */
+package com.github.exabrial.petrify.testing;
---
+++ petrify-testing/src/main/java/com/github/exabrial/petrify/testing/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Integration tests for petrify importers and compilers.
+ */
+package com.github.exabrial.petrify.testing;
